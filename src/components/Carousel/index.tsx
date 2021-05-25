694d1e0a8b56b316748b771287d73eff7874f7fb
John Freitas <john.freitasau@gmail.com>
2021-05-25 17:29:47 +1000

    Created carousel section with swipers

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,9 +1,109 @@
-import { Divider, Center } from "@chakra-ui/react";
+import {
+  Divider,
+  Center,
+  Text,
+  Flex,
+  Image,
+  Box,
+  VStack,
+  Heading,
+} from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Mousewheel,
+  Keyboard,
+} from "swiper/core";
+
+SwiperCore.use([Navigation, Pagination, Mousewheel, Keyboard]);
 
 export function Carousel() {
   return (
-    <Center>
-      <Divider width="90px" borderColor="gray.500" size="2px" />
-    </Center>
+    <Flex as="section" direction="column" w="100%" align="center">
+      <Divider w="90px" borderColor="gray.500" size="2px" />
+      <Flex direction="column" w="100%">
+        <Heading fontWeight="medium" py="4rem">
+          <VStack>
+            <Text>Let's go?</Text>
+            <Text>So, choose your continent.</Text>
+          </VStack>
+        </Heading>
+        <Flex px="100" direction="column" mb="10" position="relative">
+          <Swiper
+            cssMode={true}
+            navigation={true}
+            pagination={true}
+            mousewheel={true}
+            keyboard={true}
+            // className="mySwiper"
+          >
+            <SwiperSlide>
+              <Flex
+                as="section"
+                w="100%"
+                h="335px"
+                justify="space-around"
+                align="center"
+              >
+                <Image src="images/africa.jpeg" alt="africa" />
+
+                <VStack
+                  spacing={5}
+                  maxW={520}
+                  align="center"
+                  justify="center"
+                  color="gray.50"
+                  position="absolute"
+                >
+                  <Heading fontWeight="500">
+                    <Text>Africa</Text>
+                  </Heading>
+                  <Text fontSize="1.25rem">The oldest continent.</Text>
+                </VStack>
+              </Flex>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <Flex
+                as="section"
+                w="100%"
+                h="335px"
+                justify="space-around"
+                align="center"
+              >
+                <Image src="images/asia.jpeg" alt="asia" />
+
+                <VStack
+                  spacing={5}
+                  maxW={520}
+                  align="center"
+                  justify="center"
+                  color="gray.50"
+                  position="absolute"
+                >
+                  <Heading fontWeight="500">
+                    <Text>Asia</Text>
+                  </Heading>
+                  <Text fontSize="1.25rem">The oldest continent.</Text>
+                </VStack>
+              </Flex>
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="images/europe.jpeg" alt="asia" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="images/north-america.jpeg" alt="asia" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="images/south-america.jpeg" alt="asia" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <Image src="images/oceania.jpeg" alt="asia" />
+            </SwiperSlide>
+          </Swiper>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../../styles/theme";
+import { theme } from "../styles/theme";
+
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
+
+import "../styles/swipers.css";
 
 function MyApp({ Component, pageProps }) {
   return (
